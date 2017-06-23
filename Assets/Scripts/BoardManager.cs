64651a6a4f0e855f7fbaf1810dd0422b097951ee
patrickzhandong <zhandong1506@gmail.com>
2017-06-22 21:33:46 -0400

    init gameManager, board and players

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    public int columns = 10;
+    public int rows = 10;
+    public GameObject[] floorTiles;
+    public GameObject[] team1Players;
+    public GameObject[] team2Players;
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+
+    }
+    //RandomPosition returns a random position from our list gridPositions.
+    Vector3 RandomPosition()
+    {
+        //Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
+        int randomIndex = Random.Range(0, gridPositions.Count);
+
+        //Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+        Vector3 randomPosition = gridPositions[randomIndex];
+
+        //Remove the entry at randomIndex from the list so that it can't be re-used.
+        gridPositions.RemoveAt(randomIndex);
+
+        //Return the randomly selected Vector3 position.
+        return randomPosition;
+    }
+
+
+    //LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        //Choose a random number of objects to instantiate within the minimum and maximum limits
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        //Instantiate objects until the randomly chosen limit objectCount is reached
+        for (int i = 0; i < objectCount; i++)
+        {
+            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+            Vector3 randomPosition = RandomPosition();
+
+            //Choose a random tile from tileArray and assign it to tileChoice
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+
+            //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+
+    //SetupScene initializes our level and calls the previous functions to lay out the game board
+    public void SetupScene()
+    {
+        //Creates the outer walls and floor.
+        BoardSetup();
+
+        //Reset our list of gridpositions.
+        InitialiseList();
+        Console.Write(team1Players.Length);
+
+        Instantiate(team1Players[0], new Vector3(2f, 2f, 0f), Quaternion.identity);
+        Instantiate(team1Players[1], new Vector3(5f, 2f, 0f), Quaternion.identity);
+        Instantiate(team1Players[2], new Vector3(8f, 2f, 0f), Quaternion.identity);
+        Instantiate(team2Players[0], new Vector3(2f, 8f, 0f), Quaternion.identity);
+        Instantiate(team2Players[1], new Vector3(5f, 8f, 0f), Quaternion.identity);
+        Instantiate(team2Players[2], new Vector3(8f, 8f, 0f), Quaternion.identity);
+
+    }
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public BoardManager boardScript;
+
+	// Use this for initialization
+	void Awake () {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+		
+	}
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
